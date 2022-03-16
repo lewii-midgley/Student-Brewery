ef69fdf6f0183ea463f713d19232bfde14d8982d
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2022-03-16 13:14:22 +1300

    Updated Student Brewery

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let objectArray = [
         what: 'Bourbon and Cola (6 Pack)',
         price: 15,
         rating: '7/10',
-        image:'./diesel.png',
+        image:'./img/diesel.png',
         cps: '$1.39',
         tags: ['bourbon', 'coke', 'can', 'dirty'],
         description: 'The 8% Diesel was deadly. Those were dark days that I pray to which we shall never return. The first can was like drinking crude oil, but once your body had adjusted and your spincter had unclenched, it was way, way too easy to forget how strong it was, sip your way through 11 more of them and wake up in a dumpster outside the Hunter Centre.'
@@ -24,7 +24,7 @@ let objectArray = [
         what: 'Unkown Substance (12 Pack)',
         price: 22.99,
         rating: '1/10',
-        image:'./pg.png',
+        image:'./img/pg.png',
         cps: '$1.37',
         tags: ['pg', 'danger', 'demonjuice', 'purple'],
         description: 'Purple Goanna spits in the face of all that is holy. An artificial potion of methylated spirits and dirty chemicals, this RTD has inflicted a pain on the youth of New Zealand that even Suzie Cato could not fix. The flavours are indecipherable. With the deep purple hue of the liquid, one would assume it would be grape flavoured. It’s not even close. It just… purple flavour. It’s a mixture of sugar, paint thinner, and something weirdly metallic. Purple Goanna is mankind laughing in the face of God, standing up and declaring our freedom from the natural world. We have created something so vile, so artificial, that it is no longer recognisable as coming from God’s green earth.'
@@ -36,7 +36,7 @@ let objectArray = [
         what: 'Bavarian Premium Lager',
         price: 3.49,
         rating: '7/10',
-        image:'./Bavaria.png',
+        image:'./img/Bavaria.png',
         cps: '$1.35',
         tags: ['beer', 'bottle', 'green', 'prememiem'],
         description: 'In terms of taste, for its price range Bavaria blows the competition out of the water. While the big cans and simple labelling naturally lead you to put Bavaria in the same class as Wild Buck or Ranfurly Draught, I urge you to pour it into a glass and see that it deserves to be classed alongside Heiny or Stella. The colour and clarity are outstanding. The hop presence is light, just enough to give a little kick. Faint notes of caramel and freshly cut grass see out the refreshingly dry finish. Bavaria is a perfectly legitimate example of a European Pils Lager.'
@@ -48,7 +48,7 @@ let objectArray = [
         what: 'French full-bodied Lager (15 Pack)',
         price: 21.99,
         rating: '9/10',
-        image:'./flame.png',
+        image:'./img/flame.png',
         cps: '$1.05',
         tags: ['bottle', 'glass', 'beer', 'gold'],
         description: 'Flame (pronounced Flamé) is an imported French beer, brewed by Trappist monks at a hidden monastery near the peak of the Pyrenees alps, since the early 1300s. Legend has it that the true recipe has never been written down, instead it is passed by oral tradition from master to apprentice. Before being entrusted with the recipe, the monks must go through years of physical, mental and spiritual training to prove that they are deserving custodians. The subtle nuances of this beer’s magnificence can only truly be appreciated by the tongue of an experienced taster. The hops dance delicately on the taste buds, playfully teasing the drinker with an incredible array of sensations. The malts play an undertone that can only be described as smooth jazz on a summer evening.  '
@@ -60,7 +60,7 @@ let objectArray = [
         what: 'Premium Indian Beer (6 Pack)',
         price: 13.99,
         rating: '4/10',
-        image:'./king.png',
+        image:'./img/king.png',
         cps: '$1.29',
         tags: ['kingy', 'india', 'beer', 'strong', 'blackout'],
         description: 'This beer created in India is cheap, easy to drink, and much like its slogan (“The King of Good Times”) suggests, it’s a fucking good time. However, there’s a reason why you don’t see hordes of Uni students drinking these delectable drunken delights, it’s because these fuckers are sneaky. One minute you’re fine, then the next you’re absolutely pissed off your tits, passed out in your mate’s room, lounge or alleyway, most likely all three. The secret in Kingfisher’s deception is how easy they are to get down. They’re crisp, light and taste fine, unlike most other higher percentage beers. My theory is that much like a fish is attracted to a lure, skint students see this price, fall for the ploy, and never make the same mistake again.'
@@ -72,7 +72,7 @@ let objectArray = [
         what: 'Sauvignon Blanc 2016',
         price: 6.99,
         rating: '3/10',
-        image:'./clear.png',
+        image:'./img/clear.png',
         cps: '$0.91',
         tags: [''],
         description: 'Cleanskin Sauvignon Blanc is a playful number that dances gently on the taste buds and fills the room with laughter. Of course, by “dance,” I mean “stomp” and by “laughter” I mean “screams of pain”. Cleanskin takes all the leftover backwash from all the half-drunk glasses in all the dirty pubs around the country, chucks them into a big plastic bucket, and stirs it around until it’s just an indecipherable mess of grapes, spit, and ethanol.'
@@ -84,7 +84,7 @@ let objectArray = [
         what: 'Nelson Vodka RTDs (10 Pack)',
         price: 24.99,
         rating: '8/10',
-        image:'./glacials.png',
+        image:'./img/glacials.png',
         cps: '$1.39',
         tags: ['vodka', 'cans', 'healthy', 'fruity', 'nelson'],
         description: 'Hard seltzer, adult seltzer, mature seltzer, spiked seltzer and hard sparkling alcohol water is a type of highball drink containing carbonated water, alcohol, and often fruit flavorings. In the US the alcohol is usually made by fermenting cane sugar; sometimes malted barley is used.' 
@@ -96,14 +96,14 @@ let objectArray = [
         what: 'Death in a bottle',
         price: 10.99,
         rating: '5.5/10',
-        image:'./nitro.png',
+        image:'./img/nitro.png',
         cps: '$1.39',
         tags: [''],
         description: 'Nitro is a kick up the arse from a big, heavy boot made of pure methamphetamine. The fact that this drink even exists is evidence enough that the human race is destined to destroy itself. Let’s break it down. At its core, Nitro is a fairly simple cocktail of vodka and orange soda – an artificial screwdriver, if you will.  Then they threw both taurine and guarana in there and made it as strong as legally possible. The end product is absolutely not safe for human consumption, but it’s undeniably yum – anything with that much sugar has to be. '
     }
 ];
 
-prompt('Are you over the age of 18?');
+// prompt('Are you over the age of 18?');
 
 function objectArrayCardInfo(){
 
@@ -112,14 +112,11 @@ function objectArrayCardInfo(){
 
         generateCard(i);
     };
-
     modal();
 };
 
 
-
 objectArrayCardInfo();
-
 
 
 function modal(){
@@ -401,6 +398,7 @@ function generateCard(x){
     $('#cardContent').append(
         `
                 <div class="card card--style" style="width: 18rem;">
+
                     <div class="img-container">
                     <img class="card-img-top" src="${objectArray[x].image}" alt="Card image cap">
                     </div>
@@ -411,6 +409,7 @@ function generateCard(x){
                         <p class="card-text">Cost Per Standard Drink: ${objectArray[x].cps}</p>
                         <button id="${objectArray[x].id}" type="button" class="btn btn-primary moreInformation" data-toggle="modal" data-target="#exampleModalCenter">Review</button>
                     </div>
+                    
                 </div>  
         `
     );
@@ -423,8 +422,6 @@ function generateCard(x){
 // =====================================
 // end of card generator
 // =====================================
-
-
 
 
 
